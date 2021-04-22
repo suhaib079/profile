0fdfc93d82c17e6fdc0c3e292e3f0bed7586949b
suhaib079 <suhaibabdennabi33@gmail.com>
2021-04-22 15:52:14 +0300

    k

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
+
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`)); 
